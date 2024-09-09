5d254311cea6f436a39b4dbd7b13e4c2c126eaec
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2024-09-09 11:48:03 +0200

    add gleam extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,8 +31,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.gleam" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
