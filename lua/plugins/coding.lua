fb0f9b40ce998c64355a360989ada43551e8ef56
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2024-09-09 12:14:27 +0200

    use mona for elixir module searching

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,4 +5,20 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  {
+    "michael-a-grammar/mona.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      {
+        ";m",
+        "<cmd>Telescope mona elixir_project_modules<cr>",
+        desc = "Project Modules",
+        mode = { "n", "x" },
+        ft = "elixir",
+      },
+    },
+  },
 }
