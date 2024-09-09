7cb2df0f4db7d0d90940cfcae184a1bafb145df9
Victor Martinez <49537445+JasterV@users.noreply.github.com>
2024-09-09 11:47:52 +0200

    add configuration for lexical

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,9 @@
+local lexical = {
+  filetypes = { "elixir", "eelixir", "heex", "surface" },
+  cmd = { "/Users/victormartinez/Documents/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+  settings = {},
+}
+
 return {
   -- tools
   {
@@ -7,16 +13,13 @@ return {
         "codelldb",
         "docker-compose-language-service",
         "dockerfile-language-server",
-        "elixir-ls",
         "eslint-lsp",
         "json-lsp",
         "markdownlint",
         "prettier",
-        "rust-analyzer",
         "shfmt",
         "shellcheck",
-        -- TOML toolkit
-        "taplo",
+        "taplo", -- TOML toolkit
         "typescript-language-server",
         "astro-language-server",
         "yaml-language-server",
@@ -30,9 +33,34 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        lexical = {
+          mason = false,
+          filetypes = lexical.filetypes,
+          on_attach = function()
+            print("Lexical has started.")
+          end,
+          settings = lexical.settings,
+        },
         tailwindcss = {
           filetypes_include = { "heex", "elixir" },
         },
+      },
+      setup = {
+        lexical = function(_, _)
+          local lspconfig = require("lspconfig")
+          local configs = require("lspconfig.configs")
+          configs.lexical = {
+            default_config = {
+              filetypes = lexical.filetypes,
+              root_dir = function(fname)
+                print("finding root dir")
+                return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+              end,
+              cmd = lexical.cmd,
+            },
+          }
+          return false
+        end,
       },
     },
   },
